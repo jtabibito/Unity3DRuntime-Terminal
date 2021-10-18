83dd3232148320946be251449f5fce418e7575f7
Tabibito <34726874+jtabibito@users.noreply.github.com>
2021-10-18 17:43:51 +0800

    Delete Terminal directory

--- Terminal/OpenTerminal/Scripts/TerminalStrings.cs
+++
@@ -1,8 +0,0 @@
-namespace Terminal {
-    public class TerminalStrings
-    {
-        public const string COMMAND_NOT_FOUND = "Command not found! type \"help\" for list of available commands!";
-        public const string MULTIPLE_COMMAND_NAMES = "Multiple commands are defined with: ";
-        public const string ARGUMENT_COUNT_MISSMATCH = "Method {0} needs {1} arguments, passed {2}";
-    }
-}
